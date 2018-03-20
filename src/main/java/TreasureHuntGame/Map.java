5d073c4121e7f71472d6f190034fd82ad8b10708
Jasmine Xuereb <jasmine.xuereb.15@um.edu.mt>
2018-03-20 13:25:47 +0100

    map class and correspoding junit tests

--- src/main/java/TreasureHuntGame/Map.java
+++ src/main/java/TreasureHuntGame/Map.java
@@ -1,7 +1,47 @@
 package TreasureHuntGame;
+import java.util.*;
 
-/**
- * Created by jasmi on 20/03/2018.
- */
 public class Map {
+
+    private int size;
+    private double waterProb = 0.1;
+
+    public Map(){
+    }
+
+    public int GetSize(){
+        return size;
+    }
+
+    public void SetSize(int s){
+        size = s;
+    }
+
+    public char[][] CreateMap(int s){
+        size = s;
+        char mapArray[][] = new char[size][size];
+        Random rand = new Random();
+        double temp;
+        int x, y;
+
+        //determine whether a given tile is a water tile using probability
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                temp = rand.nextDouble();
+                if(temp < waterProb){ //then it is a water tile
+                    mapArray[i][j] = 'W';
+                }
+                else {
+                    mapArray[i][j] = 'G';
+                }
+            }
+        }//close for loop
+
+        //find a random position for the treasure
+        x = rand.nextInt(size);
+        y = rand.nextInt(size);
+        mapArray[x][y] = 'T';
+
+        return mapArray;
+    }
 }
