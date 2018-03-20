e101a31313c85149a119ffbdcf604c36a7eee6d9
Jasmine Xuereb <jasmine.xuereb.15@um.edu.mt>
2018-03-20 14:03:21 +0100

    game class

---
+++ src/main/java/TreasureHuntGame/Game.java
@@ -0,0 +1,32 @@
+package TreasureHuntGame;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jasmi on 20/03/2018.
+ */
+
+public class Game {
+    int numOfPlayers;
+    int mapSize;
+    ArrayList<Player> players = new ArrayList<Player>();
+
+    //jas
+    char[][] GenerateMap(){
+    }
+
+    //jas
+    void GenerateStartingPositions(char [][] map){
+        //modifies players
+    }
+
+    //skip
+    void GenerateHtmlFile(){
+
+    }
+
+    //marc
+    int CheckMovement(){
+
+    }
+}
