9e1baf687c85e132f13281771e80e5ad4f44c5f6
mfer0010 <marc.ferriggi.15@um.edu.mt>
2018-03-20 10:40:32 +0100

    Fixed Unit Test Files

--- src/main/java/TreasureHuntGame/HelloWorld.java
+++ src/main/java/TreasureHuntGame/HelloWorld.java
@@ -1,4 +1,4 @@
-package main;
+package TreasureHuntGame;
 
 public class HelloWorld {
 
--- src/test/java/TreasureHuntGame/HelloWorldTests.java
+++ src/test/java/TreasureHuntGame/HelloWorldTests.java
@@ -1,4 +1,4 @@
-package tests;
+package TreasureHuntGame;
 
 import org.junit.After;
 import org.junit.Before;
